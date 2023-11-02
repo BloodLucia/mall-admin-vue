da9f18495d2e207157ae33f7d84194300febd6b9
Kalougata <kalougata@111.com>
2023-11-02 11:04:13 +0800

    custom request util

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+type JSONValue =
+  | null
+  | boolean
+  | string
+  | number
+  | JSONValue[]
+  | Record<string, JSONValue>
---
+++ src/utils/request.ts
@@ -0,0 +1,33 @@
+import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+
+type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
+type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
+type DeleteConfig = Omit<AxiosRequestConfig, 'params'>
+
+class Request {
+    instance: AxiosInstance
+    constructor(baseURL: string) {
+        this.instance = axios.create({
+            baseURL
+        })
+    }
+    get<R = unknown>(url: string, query?: Record<string, JSONValue>, config?: GetConfig) {
+        return this.instance.request<R>({ ...config, url: url, params: query, method: 'get' })
+      }
+      post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig) {
+        return this.instance.request<R>({ ...config, url, data, method: 'post' })
+      }
+      patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig) {
+        return this.instance.request<R>({ ...config, url, data, method: 'patch' })
+      }
+      delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig) {
+        return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
+      }
+}
+
+const baseURL = import.meta.env.DEV ? "http://127.0.0.1:8001/api/v1/admin" : ""
+const request = new Request(baseURL)
+
+export default request

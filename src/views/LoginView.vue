a48d0f0b4ab350483375e0f0ace55662deef4c11
Kalougata <kalougata@111.com>
2023-11-02 09:54:52 +0800

    add LoginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input type="text" v-model="output">
+        <h1>{{ outputComputed }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const output = ref("")
+const outputComputed = computed(() => output.value.toUpperCase())
+</script>
+
+<style scoped lang="scss">
+
+</style>
